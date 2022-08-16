d3cd979ec66fef6503a30302e1ba2a739ed8f196
Ayush <ayushbhardwaj.1530@gmail.com>
2022-08-17 01:15:57 +0530

    Bubble Sort

---
+++ BubbleSort.cpp
@@ -0,0 +1,18 @@
+// BUBBLE SORT
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        int n = nums.size();
+        for(int i=0; i<n; i++){
+            bool flag = false; // used to break out when there will be no swaps throughout that means array is sorted
+            for(int j=0; j<n-i-1; j++){ // ran till n-1-i to improve complexity because every time greatest element reaches its correct position
+                if(nums[j]>nums[j+1]){
+                     swap(nums[j],nums[j+1]);
+                    flag = true; 
+                }
+            }
+            if(!flag) break;
+        }
+        return nums;
+    }
+};
